0887a1ea7677e8869e14e8fe27b2257579b9327d
ihsanmoes <ihsanmoes@gmail.com>
2025-06-25 05:58:40 +0700

    tambah data mahasiswa

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Informatika</title>
+</head>
+<body>
+    <h1 align="center" > WEB INFORMATIKA </h1>
+    <hr width="50%" align="center" size="5px" color="darkgrey">
+ 
+    <nav>
+        <ul style="list-style-type: none; text-align: center; padding: 0%;">
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+            <a href="home.html">Home</a></li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+            <a href="about.html">About</a></li>   
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+            <a href="service.html">Services</a></li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+            <a href="contact.html">Contact</a></li>    
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+            <a href="login.html">Login</a></li>
+            <li style="display: inline; margin: 0% 15px; text-decoration: none;">
+            <a href="datamahasiswa.php">Data Mahasiswa</a></li>                                                  
+        </ul>
+    </nav>
+    
+    <hr width="50%" align="center" size="2px" color="black">
+    <h1 align="center">
+        <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2qWiI_GcRYeirhf1_ED22Qjf7v5gvG38iHg&s"
+        height="200px">
+    </h1>
+    <p>
+        <b>Lorem ipsum</b> dolor, <i>sit amet</i> consectetur adipisicing elit. 
+        Quidem cum sapiente animi eligendi temporibus dolores itaque, 
+        maiores eum <u>perferendis reprehenderit?</u> In labore perspiciatis 
+        necessitatibus? Sunt reprehenderit ipsa voluptatum dignissimos? 
+        Eos qui debitis vel harum nihil. Impedit, porro, earum molestiae 
+        exercitationem sed adipisci sequi officiis, aperiam iusto vitae 
+        odit laborum fugiat?
+    </p>
+    <h2>Daftar Belanja</h2>
+    <ol> 
+        <li>Makanan
+            <ul>
+                <li>Bumbu Pawon</li>
+                <li>Bawang Merah</li>
+                <li>Bawang Putih</li>
+            </ul>
+        </li>
+        <li>Minuman
+            <ul>
+                <li>Coca Cola</li>
+                <li>Fanta</li>
+                <li>Sprite</li>
+                <li>Aqua</li>
+            </ul>
+        </li>
+        <li>Ikan
+            <ul>
+                <li>Lele</li>
+                <li>Gurame</li>
+                <li>Bandeng</li>
+            </ul>
+        </li>
+    </ol>
+</body>
+</html>
